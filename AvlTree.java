f7dc91afa31aad12b1404b613aec7d8d48b918aa
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-20 16:54:21 +1200

    added rotateRight method

--- AvlTree.java
+++ AvlTree.java
@@ -2,16 +2,28 @@ public class AvlTree {
 
   Node root;
 
-  int getHeight(Node node) {
-    if (node == null){
-        return 0;
+  private int getHeight(Node node) {
+    if (node == null) {
+      return 0;
     }
     return node.height;
   }
 
-  int getBalance(Node node) {
-    if (node == null)
-        return 0;
+  private int getBalance(Node node) {
+    if (node == null) return 0;
     return getHeight(node.left) - getHeight(node.right);
-}
+  }
+
+  private Node rotateRight(Node node) {
+    Node leftNode = node.left;
+    Node middle = leftNode.right;
+
+    leftNode.right = node;
+    node.left = middle;
+
+    node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    leftNode.height = Math.max(getHeight(leftNode.left), getHeight(leftNode.right)) + 1;
+
+    return leftNode;
+  }
 }
