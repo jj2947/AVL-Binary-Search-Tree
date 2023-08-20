8b3c672e6bd30c05b9619685f9d0629aebcc2544
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-20 18:28:35 +1200

    added test values

--- Main.java
+++ Main.java
@@ -3,6 +3,15 @@ public class Main {
   public static void main(String[] args) {
 
     AvlTree tree = new AvlTree();
+
+    int[] valuesToAdd = {1, 2, 3, 4, 5, 6, 7};
+
+    for (int value : valuesToAdd) {
+      tree.add(value);
+      System.out.println("After adding " + value + ":");
+      System.out.println(tree.toString());
+    }
+
     int n = 1000000;
     for (int i = 0; i < n; i++) {
       tree.add(i);
