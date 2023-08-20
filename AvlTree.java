f36d7b9668b0419ca47c6af746aeebc3ef494cda
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-20 16:37:45 +1200

    made a Node class

--- AvlTree.java
+++ AvlTree.java
@@ -1,3 +1,6 @@
 public class AvlTree {
-    
+
+    public void add (int value) {
+        root = insert(root, key);
+    }
 }
---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+  int value, height;
+  Node left, right;
+
+  Node(int value) {
+    this.value = value;
+    this.height = 1;
+  }
+}
